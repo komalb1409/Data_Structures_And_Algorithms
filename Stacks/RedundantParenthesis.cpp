7b342b4fbbed24e580923db3ef4828648662663d
Komal <komalb1409@gmail.com>
2021-08-04 16:49:21 +0530

    RedundantParenthesisCheck

---
+++ Stacks/RedundantParenthesis.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    bool ans =  false;
+    stack <int> st;
+    for(int i = 0 ; i <s.size() ;i++)
+    {
+        if(s[i] == '+' or s[i] == '-' or s[i] == '*' or s[i] == '/' or s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+
+
+        else if(s[i] == ')')
+        {
+            if(st.top() == '('){
+                ans = true;
+            }
+            while(st.top() == '+' or st.top() == '-' or st.top() == '*' or st.top() == '/')
+            {
+                st.pop();
+            }
+           st.pop();
+        }
+    }
+
+    cout<<ans;
+
+}
