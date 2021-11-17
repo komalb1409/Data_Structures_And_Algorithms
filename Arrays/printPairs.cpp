9059954c4736c6534f071800afdfb876a223920f
Komal <komalb1409@gmail.com>
2021-11-17 23:13:01 +0530

    PrintPairs

---
+++ Arrays/printPairs.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void printPairs(int arr[], int n)
+{
+    for(int i = 0 ; i < n ; i++)
+    {
+        for(int j = i + 1 ; j < n ; j++)
+        {
+            cout<<"("<<arr[i]<<","<<arr[j]<<")"<<"\n";
+        }
+    }
+}
+
+
+int main()
+{
+    int arr[] = {10,20,30,40,50};
+    int n = sizeof(arr)/sizeof(int);
+    printPairs(arr, n);
+    return 0;
+}
