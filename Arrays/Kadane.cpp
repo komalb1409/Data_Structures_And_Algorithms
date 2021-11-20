392b2d79b73449fccc455caa3d81c4fe800bfe13
Komal <komalb1409@gmail.com>
2021-11-21 01:06:09 +0530

    Kadane

--- Arrays/Kadane.cpp
+++ Arrays/Kadane.cpp
@@ -1,20 +1,19 @@
 #include<iostream>
 #include<climits>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int arr [] = {1,-2,3,4,4,-2};
-    int currentSum = 0;
-    int maxSum = INT_MIN;
+    int currentSum = arr[0];
+    int maxSum = arr[0];
 
-    for(int i = 0 ; i<6 ;i++)
+
+    for(int i = 1 ; i<6 ;i++)
     {
-        currentSum+=arr[i];
-        if(currentSum<0){
-            currentSum = 0;
-        }
-        maxSum = max(maxSum,currentSum);
+        currentSum = max(arr[i], currentSum+arr[i]);
+        maxSum = max(maxSum, currentSum);
     }
     cout<<maxSum<<endl;
 
